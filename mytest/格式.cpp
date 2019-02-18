b95a2ad1c66a1a109281d51c541ec30df6c78003
simzhangbest <1530152313@qq.com>
2019-02-18 17:19:21 +0800

    split string

--- mytest/格式.cpp
+++
@@ -1,7 +0,0 @@
-def createCounter():
- count = 0
-def counter():
- nonlocal count
- count += 1
- return count
-return counter
---
+++ 练习/string_split.cpp
@@ -0,0 +1,48 @@
+
+
+/*
+ *  string split
+ * */
+
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <vector>
+#include <sstream>
+//分隔符只会一次分割
+std::vector<std::string> split(const std::string& s, char delimiter)
+{
+    std::vector<std::string> tokens;
+    std::string token;
+    std::istringstream tokenStream(s);
+    while (std::getline(tokenStream, token, delimiter))
+    {
+        printf("get token[0] : %d \n", token[0]);
+        tokens.push_back(token);
+    }
+    return tokens;
+}
+// 分隔符会多次分割
+std::vector<std::string> split_multi(const std::string& s, char delimiter)
+{
+    std::vector<std::string> tokens;
+    std::string token;
+    std::istringstream tokenStream(s);
+    while (std::getline(tokenStream, token, delimiter))
+    {
+        if (token[0] != 0x00) {
+            printf("get token[0] : %d \n", token[0]);
+            tokens.push_back(token);
+        }
+    }
+    return tokens;
+}
+
+int main() {
+    std::vector<std::string> res;
+    res = split_multi("ni%%%hao%haha%%%%%%ni", '%');
+    std::vector<std::string>::iterator it;
+    for(it = res.begin(); it != res.end(); it++) {
+        std::cout << "res:" << *it << std::endl;
+    }
+}
