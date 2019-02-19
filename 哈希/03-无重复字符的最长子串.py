1c9bdc76d01edab8225f2fa1725b3d0895f87690
mingya.zhang <mingya.zhang@hobot.cc>
2019-02-19 22:12:59 +0800

    修改代码仓库文件，使其归档

--- 哈希/03-无重复字符的最长子串.py
+++ 哈希/03-无重复字符的最长子串.py
@@ -15,5 +15,19 @@ class Solution(object):
                 res = res[index + 1:] + ch
         return max(maxlen, len(res))
 
+    def lengthOfLongestSubstring_better(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        st = {}
+        i, ans = 0, 0
+        for j in range(len(s)):
+            if s[j] in st:
+                i = max(st[s[j]], i)
+            ans = max(ans, j - i + 1)
+            st[s[j]] = j + 1
+        return ans;
+
 solve = Solution()
 print(solve.lengthOfLongestSubstring("abcabcdac"))
